993939f6f9e7bca48395153741ae2699e8a0fc1d
Naman Ajmera <cool.naman.ajmera@gmail.com>
2023-09-26 12:04:05 +0530

    Added:Day 3 To Be Or Not To Be.

--- Day-3-ToBeOrNotToBe.js
+++ Day-3-ToBeOrNotToBe.js
@@ -1,20 +1,22 @@
 var expect = function (val) {
-  return {
-    toBe: (val2) => {
-      if (val !== val2) {
-        throw new Error("Not Equal");
-      } else {
-        return true;
-      }
-    },
+  const toBe = (val2) => {
+    if (val !== val2) {
+      throw new Error("Not Equal");
+    } else {
+      return true;
+    }
+  };
 
-    notToBe: (val3) => {
-      if (val === val3) {
-        throw new Error("Equal");
-      } else {
-        return true;
-      }
-    },
+  const notToBe = (val3) => {
+    if (val === val3) {
+      throw new Error("Equal");
+    } else {
+      return true;
+    }
+  };
+  return {
+    toBe,
+    notToBe,
   };
 };
 
